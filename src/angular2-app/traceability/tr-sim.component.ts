3a41e9217fe4113872ce85b890a53d3c839933c5
Tobias Frejo <tobias@frejo.dk>
2023-05-18 20:45:46 +0200

    Detailed simulation overview view

--- src/angular2-app/traceability/tr-sim.component.ts
+++ src/angular2-app/traceability/tr-sim.component.ts
@@ -1,49 +1,75 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import IntoCpsApp from "../../IntoCpsApp";
-import { Activity, TrNode, Trace } from "../../traceability/models";
+import { Activity, Agent, Artefact, Tool, TrNode, Trace } from "../../traceability/models";
 import { TraceabilityController } from "../../traceability/TraceabilityController";
+import { ArtefactType } from "../../traceability/TraceabilityKeys";
+import { IntoCpsAppMenuHandler } from "../../IntoCpsAppMenuHandler";
+import * as path from 'path'
 
 @Component({
     selector: "tr-sim",
     templateUrl: './angular2-app/traceability/tr-sim.component.html'
 })
-export class TrSimulationComponent {
+export class TrSimulationComponent implements OnInit {
     @Input() 
     node: Activity;
 
-    result: Array<TrNode>
+    //result: Array<TrNode>
     loading: Boolean
     TrController: TraceabilityController
 
+    fmus: Array<Artefact>
+    mm_artefact: Artefact
+    sim_artefact: Artefact
+    engine: Tool
+    agent: Agent
+    result_artefact: Artefact
+
     constructor () {
-        this.result = []
-        this.loading = false
+        //this.result = []
+        this.loading = true
         this.TrController = IntoCpsApp.getInstance().trController
-    
-        console.log("Initialized sim component: " + this.node)
+
+        console.log("Constructed sim component: " + this.node)
     }
 
-    getFmus() {
-        this.loading = true
-        this.TrController.client.getFmusInSimulation(this.node.uri)
-        .then((rd: {nodes: Array<TrNode>, traces: Array<Trace>}) => {
-            let used: Array<string> = []
-
-            this.result = []
-
-            // Get all "used" traces
-            rd.traces.forEach(tr => {
-                console.log(tr)
-                if (!used.includes(tr.object) && tr.predicate == "prov:used") used.push(tr.object)
-            })
-
-            // Get the FMU nodes with a used trace to it
-            rd.nodes.forEach(nd => {
-                if (used.includes(nd.uri)) this.result.push(nd)
-            })
-        })
+    public ngOnInit(): void {
+        console.log("OnInit sim component: " + JSON.stringify(this.node))
+        this.getSimTraces()
         .finally(() => {
             this.loading = false
         })
+    }
+
+    getSimTraces() {
+        return this.TrController.client.getSimulationDetails(this.node.uri)
+        .then(res => {
+            console.log("Received", JSON.stringify(res, null, 2))
+
+            this.fmus = res.fmus;
+            this.mm_artefact = res.mmConfig;
+            this.sim_artefact = res.simulationConfig;
+            this.engine = res.engine;
+            this.agent = res.agent;
+            this.result_artefact = res.result;
+        })
+    }
+
+    openModel() {
+        IntoCpsAppMenuHandler.getInstance().openMultiModel(
+            path.resolve(
+                IntoCpsApp.getInstance().activeProject.getRootFilePath(),
+                this.mm_artefact.path
+            )
+        )
+    }
+
+    openSim() {
+        IntoCpsAppMenuHandler.getInstance().openCoeView(
+            path.resolve(
+                IntoCpsApp.getInstance().activeProject.getRootFilePath(),
+                this.sim_artefact.path
+            )
+        )
     }
 }
